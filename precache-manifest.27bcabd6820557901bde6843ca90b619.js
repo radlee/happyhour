936e2d99555f696a3d78f5435c388a96e1247f20
radlee <prof.lolito@gmail.com>
2020-08-26 00:52:27 +0200

    Updates

---
+++ precache-manifest.27bcabd6820557901bde6843ca90b619.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e79b78b47c1e33561f70c5454fc98f2",
+    "url": "/happyhour/index.html"
+  },
+  {
+    "revision": "29028927ee79469d4f5a",
+    "url": "/happyhour/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0aee89ff18b46330f298",
+    "url": "/happyhour/static/css/main.fb707286.chunk.css"
+  },
+  {
+    "revision": "29028927ee79469d4f5a",
+    "url": "/happyhour/static/js/2.935201e5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/happyhour/static/js/2.935201e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aee89ff18b46330f298",
+    "url": "/happyhour/static/js/main.9e05813c.chunk.js"
+  },
+  {
+    "revision": "e76789381b040c1c1e50",
+    "url": "/happyhour/static/js/runtime-main.85461974.js"
+  },
+  {
+    "revision": "105350c582905ebaf0e1bb5858899848",
+    "url": "/happyhour/static/media/bg1.105350c5.jpg"
+  },
+  {
+    "revision": "105350c582905ebaf0e1bb5858899848",
+    "url": "/happyhour/static/media/bg2.105350c5.jpg"
+  },
+  {
+    "revision": "f17b78468101cd1e8f0a198bdb756513",
+    "url": "/happyhour/static/media/bg3.f17b7846.jpg"
+  },
+  {
+    "revision": "33235993e202013c2e9b39de58775628",
+    "url": "/happyhour/static/media/bg4.33235993.jpg"
+  },
+  {
+    "revision": "b0b095ad7fca961a49ff9a18db436334",
+    "url": "/happyhour/static/media/bsponsor.b0b095ad.png"
+  },
+  {
+    "revision": "028532d0592eb0deaf89a1607b4120d5",
+    "url": "/happyhour/static/media/circles.028532d0.png"
+  },
+  {
+    "revision": "808d6ac027d33b19cefe5d81f2662ef5",
+    "url": "/happyhour/static/media/hhf1.808d6ac0.jpg"
+  },
+  {
+    "revision": "af65d585537c97866f78d8ea45a9d1ed",
+    "url": "/happyhour/static/media/hhf2.af65d585.jpg"
+  },
+  {
+    "revision": "fc0a33a2d6c1d15874c0bb5312f81d2e",
+    "url": "/happyhour/static/media/inno.fc0a33a2.jpg"
+  },
+  {
+    "revision": "cd16e71c85ae46fbf4856cb7c862f5fa",
+    "url": "/happyhour/static/media/is.cd16e71c.jpg"
+  },
+  {
+    "revision": "4c253aa319d3556b86d8f1e26735c6d6",
+    "url": "/happyhour/static/media/logo512.4c253aa3.png"
+  },
+  {
+    "revision": "4f35f93bc949f88b6c39325fec695c21",
+    "url": "/happyhour/static/media/sponsor.4f35f93b.jpg"
+  }
+]);
