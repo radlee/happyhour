34215d13931c7a4860808d6bfa2fa25141177e6b
radlee <prof.lolito@gmail.com>
2020-09-05 09:46:17 +0200

    Address

--- src/components/contact-us/contact.component.jsx
+++ src/components/contact-us/contact.component.jsx
@@ -13,6 +13,9 @@ class ContactForm extends Component {
                     <h2 className="insta">Email: hhourfoundation@gmail.com</h2>
                     <h2 className="insta">Facebook: @hhourfoundation</h2>
                     <h2 className="insta">Instagram: @hhourfoundation</h2>
+                    <br />
+                    <h2 className="insta">Room 31792 Rubbertree Court | N2 Gateway | Delft (7100) | Western Cape </h2>
+
                 </Container>
             </div>
 
